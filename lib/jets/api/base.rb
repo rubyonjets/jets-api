d2352835919ecbc030c9e4f23194a6ab191036a5
Tung Nguyen <tongueroo@gmail.com>
2024-01-16 21:13:42 +0000

    Jets.project.name

--- lib/jets/api/base.rb
+++ lib/jets/api/base.rb
@@ -10,10 +10,10 @@ module Jets::Api
         params = {}
         params[:jets_env] = Jets.env.to_s
         params[:jets_extra] = Jets.extra.to_s if Jets.extra
-        params[:name] = Jets.project_namespace
+        params[:name] = Jets.project.namespace
         params[:region] = Jets.aws.region
         params[:account] = Jets.aws.account
-        params[:project_id] = Jets.project_name
+        params[:project_id] = Jets.project.name
         params[:jets_api_version] = Jets::Api::VERSION
         params[:jets_version] = Jets::VERSION
         params[:ruby_version] = RUBY_VERSION

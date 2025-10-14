ce8de50e5556ae21102c4ba45297d17966d9837d
Tung Nguyen <tongueroo@gmail.com>
2025-10-14 00:48:27 +0000

    remove report without threads for http ssl true thread safety

--- lib/jets/api/gems/report.rb
+++ lib/jets/api/gems/report.rb
@@ -9,15 +9,9 @@ class Jets::Api::Gems
     end
 
     def report(gems)
-      threads = []
       gems.each do |gem_name|
-        threads << Thread.new do
-          Jets::Api::Gems.report(gem_name: gem_name)
-        end
+        Jets::Api::Gems.report(gem_name: gem_name)
       end
-      # Wait for request to finish because the command might finish before
-      # the Threads even send the request. So we join them just case
-      threads.each(&:join)
     end
   end
 end
